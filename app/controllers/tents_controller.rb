5357a3742b101751648e8b01db3aaf4baeacfd3c
james hsia <jameshsia@hotmail.co.uk>
2022-05-30 17:48:55 +0100

    comment

---
+++ app/controllers/tents_controller.rb
@@ -0,0 +1,36 @@
+class TentsController < ApplicationController
+
+  def index
+    @tents = Tent.all
+  end
+
+  def show
+    @tent = Tent.find(params[:id])
+    @booking = Booking.new
+  end
+
+  # def new
+  #   @tent = Tent.new
+  # end
+
+#   def create
+#     @tent = Tent.new(list_params)
+#     if @tent.save
+#       redirect_to tent_path(@tent)
+#     else
+#       render :new, status: :unprocessable_entity
+#     end
+#   end
+
+#   def destroy
+#     @tent = Tent.find(params[:id])
+#     @tent.destroy
+#     redirect_to tents_path, status: :see_other
+#   end
+
+#   private
+
+#   def list_params
+#     params.require(:tent).permit(:title, :description, :capacity, :location, :price)
+#   end
+end
