f9b3ce14fd9abaa27d1f579b8dbd2755192b244c
james hsia <jameshsia@hotmail.co.uk>
2022-06-01 15:00:31 +0100

    update homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @tent = Tent.new
-    @bookings = Booking.all
+    # @tent = Tent.new
+    # @bookings = Booking.all
+    @tents = Tent.last(3)
   end
 end
