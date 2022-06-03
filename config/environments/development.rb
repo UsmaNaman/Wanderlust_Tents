58128b51fce64c763d2c21dbcdd69a7f30b06bb1
Melina Kim <melinamkim@gmail.com>
2022-06-03 15:56:37 +0100

    font bolder

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,8 +58,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
-  # Raises error for missing translations.
+  config.assets.check_precompiled_asset = false
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
