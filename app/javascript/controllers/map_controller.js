251992b289100fa35d270be793a092043d5f8056
Usman Akram <uakram86@gmail.com>
2022-06-03 12:41:22 +0100

    sorted styling of maps on bookings show page

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -18,9 +18,10 @@ export default class extends Controller {
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
-
-    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl }))
+    if (this.element.dataset.search === "true"){
+      this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl }));
+    }
   }
 
   #addMarkersToMap() {
